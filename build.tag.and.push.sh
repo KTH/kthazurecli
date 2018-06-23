15fb4521206a93715a93916584edbaa35e6a4694
Michael Tsiortos <michael@kth.se>
2018-06-23 13:27:43 +0200

    some docker clieanup before building

--- build.tag.and.push.sh
+++ build.tag.and.push.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 CURRENT=$(date "+%Y%m%d%H%M")
 
+# Remove unused images
+docker image prune
+
+# Remove stopped containers.    
+docker container prune
+
+# Remove unused volumes
+docker volume prune
+
+# Remove unused networks
+docker network prune
+
+# Command to run all prunes:  Use with caution
+#docker system prune
+
+#Pulling the latest debian:jessie
 docker pull debian:jessie
 
+#Building
 docker build --compress -t kthazurecli:$CURRENT -t kthazurecli:latest .
 
 #Tagging images for publishing to dockerhub
